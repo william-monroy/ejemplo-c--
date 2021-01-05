0f695fc7966c7bc85dd796721dc6ed7b1e50ffdb
William Monroy <A00829796@itesm.mx>
2021-01-05 02:21:42 -0500

    funcion

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 #include <iostream>
 using namespace std;
 
+void hi(){
+    cout << "HOLAAAAAAAAAA" << endl;
+}
+
 int main(){
     string ejemplo;
     cout << "Adios mundo!" << endl;
     cin >> ejemplo;
     cout << "Texto: " << ejemplo << endl;
+    hi();
     return 0;
 }
