2545eb0ee912e8db2a96b6a3da81c6365b2fbce9
Patricio Mendoza <patriciomendozapasapera@gmail.com>
2021-01-04 23:29:29 -0700

    Cambie el mensaje a adios mundo

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     string ejemplo;
-    cout << "Hola mundo!" << endl;
+    cout << "Adios mundo!" << endl;
     cin >> ejemplo;
     cout << ejemplo << endl;
     return 0;
