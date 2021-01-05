b0132547037c0a71403fbd0ebb0e71580f90a774
Patricio Mendoza <patriciomendozapasapera@gmail.com>
2021-01-04 23:28:25 -0700

    Primera Version

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(){
+    string ejemplo;
+    cout << "Hola mundo!" << endl;
+    cin >> ejemplo;
+    cout << ejemplo << endl;
+    return 0;
+}
